7de9621f24bfcd95f480e40bd3414d3bf5e94198
Eugene Wang <fengbaoxp@gmail.com>
2018-07-01 14:42:12 +0800

    初始化样例，添加licensing-service模块。

---
+++ licensing-service/src/main/java/org/eugene/licenses/controllers/LicenseController.java
@@ -0,0 +1,25 @@
+package org.eugene.licenses.controllers;
+
+import org.eugene.licenses.models.License;
+import org.eugene.licenses.services.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/organizations/{organizationId}/licenses")
+public class LicenseController {
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+    public License getLicenses(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+        return new License()
+                .withId(licenseId)
+                .withOrganizationId(organizationId)
+                .withProductName("Teleco")
+                .withLicenseType("Seat");
+    }
+}
---
+++ licensing-service/src/main/java/org/eugene/licenses/services/LicenseService.java
@@ -0,0 +1,7 @@
+package org.eugene.licenses.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LicenseService {
+}
